baeaef2aa8d7ced925968feb8f53b5e3a66014de
Jun Song Park <jp6ax@virginia.edu>
2022-04-26 13:56:45 -0400

    Update Config.php

--- classes/Config.php
+++ classes/Config.php
@@ -3,8 +3,8 @@
     class Config{
         public static $db = [
             "host" =>"localhost",
-            "user"=>"yp9ar",
-            "pass"=>"c-Ah5nywi8h6",
-            "database"=>"yp9ar",
+            "user"=>"root",
+            "pass"=>"",
+            "database"=>"test",
         ];
     }
